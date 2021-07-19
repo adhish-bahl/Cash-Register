362ad4b485d9ecbfceab1c98e9238a0277295457
adhish-bahl <adhishbahl0@gmail.com>
2021-07-19 23:41:22 +0530

    heading para done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Cash Register Manager</h2>
+      <p>Enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
     </div>
   );
 }

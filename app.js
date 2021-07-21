2c1e24b89089a273f70346d8ebb5987a3a41beb4
Adhish Bahl <adhishbahl0@gmail.com>
2021-07-21 22:12:20 +0530

    next button event done

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const billAmount = document.querySelector("#billAmount");
 const nextBtn = document.querySelector(".nextBtn");
 const messageInvalidAmount = document.querySelector("#messageInvalidAmount");
-const cashGivenForm = document.querySelector(".cashGiven");
+const cashGivenForm = document.querySelector(".cashGivenForm");
 const cashGiven = document.querySelector("#cashGiven");
 const checkBtn = document.querySelector(".checkBtn");
 const lessCashThanAmount = document.querySelector("#lessCashThanAmount");
 const invalidCashBillAmount = document.querySelector("#invalidCashBillAmount");
-const table = document.querySelector(".table");
+const outputTable = document.querySelector(".outputTable");
 const N2000 = document.querySelector(".N2000");
 const N500 = document.querySelector(".N500");
 const N100 = document.querySelector(".N100");
@@ -15,4 +15,20 @@ const N10 = document.querySelector(".N10");
 const N5 = document.querySelector(".N5");
 const N1 = document.querySelector(".N1");
 
+messageInvalidAmount.style.display = "none";
+cashGivenForm.style.display = "none";
+lessCashThanAmount.style.display = "none";
+invalidCashBillAmount.style.display = "none";
+outputTable.style.display = "none";
 
+nextBtn.addEventListener("click", ()=>{
+    let billCost = billAmount.value;
+    console.log(billCost);
+    if(billCost <= 0) {
+        messageInvalidAmount.style.display="block";
+        billAmount.innerText("");
+    } else {
+        messageInvalidAmount.style.display="none";
+        cashGivenForm.style.display = "block";
+    }
+})

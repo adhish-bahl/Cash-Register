d40775d42fee818210c902129e3e5afdba3a9f4d
Adhish Bahl <adhishbahl0@gmail.com>
2021-07-21 18:43:59 +0530

    selected all selectors

---
+++ app.js
@@ -0,0 +1,18 @@
+const billAmount = document.querySelector("#billAmount");
+const nextBtn = document.querySelector(".nextBtn");
+const messageInvalidAmount = document.querySelector("#messageInvalidAmount");
+const cashGivenForm = document.querySelector(".cashGiven");
+const cashGiven = document.querySelector("#cashGiven");
+const checkBtn = document.querySelector(".checkBtn");
+const lessCashThanAmount = document.querySelector("#lessCashThanAmount");
+const invalidCashBillAmount = document.querySelector("#invalidCashBillAmount");
+const table = document.querySelector(".table");
+const N2000 = document.querySelector(".N2000");
+const N500 = document.querySelector(".N500");
+const N100 = document.querySelector(".N100");
+const N20 = document.querySelector(".N20");
+const N10 = document.querySelector(".N10");
+const N5 = document.querySelector(".N5");
+const N1 = document.querySelector(".N1");
+
+
